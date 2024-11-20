47c397ce48bf37d10573a2664506d5b069fb07c7
Luis Fernando <guffenix@gmail.com>
2024-11-19 20:02:28 -0500

    fix: global ts

--- global.d.ts
+++ global.d.ts
@@ -1,19 +1,17 @@
-export {}
+interface TelegramWebApp {
+  initData: string
+  initDataUnsafe: Record<string, any>
+  close: () => void
+  ready: () => void
+  sendData: (data: string) => void
+  expand: () => void
+  onEvent: (eventType: string, callback: () => void) => void
+  offEvent: (eventType: string, callback: () => void) => void
+  platform?: string // Add platform property
+}
 
-declare global {
-  interface Window {
-    Telegram: {
-      WebApp: {
-        initData: string
-        initDataUnsafe: Record<string, any>
-        close: () => void
-        ready: () => void
-        sendData: (data: string) => void
-        expand: () => void
-        onEvent: (eventType: string, callback: () => void) => void
-        offEvent: (eventType: string, callback: () => void) => void
-        // Agrega más métodos si los necesitas
-      }
-    }
+interface Window {
+  Telegram: {
+    WebApp: TelegramWebApp
   }
 }
