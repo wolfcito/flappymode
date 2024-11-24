42c604f573d0da9b523a8fe6aa51ee6772fb0332
Luis Fernando <guffenix@gmail.com>
2024-11-23 23:12:50 -0500

    feat: menu pages

--- src/components/nav-menu/nav-menu.component.tsx
+++ src/components/nav-menu/nav-menu.component.tsx
@@ -1,53 +1,60 @@
-import { Home, Star, Flame, Coins, Users } from 'lucide-react'
+import Image from 'next/image'
 
 export function NavMenu() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-zinc-900 p-4 backdrop-blur-lg z-20">
-      <nav className="mx-auto max-w-md">
-        <ul className="flex items-center justify-between">
+    <div
+      className="fixed bottom-0 left-0 right-0 bg-white/10"
+      style={{
+        backgroundImage: `url('https://res.cloudinary.com/guffenix/image/upload/f_auto,q_auto/v1/flappymode/bgmarine')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <nav className="mx-auto max-w-md bg-zinc-900 backdrop-blur-sm py-2">
+        <ul className="flex items-center justify-around">
           <li>
             <a
               href="#"
-              className="flex flex-col items-center gap-1 text-zinc-400 transition-colors hover:text-zinc-100"
+              className="flex flex-col items-center text-zinc-400 transition-colors hover:text-zinc-100"
             >
-              <Home className="h-6 w-6" />
-              <span className="text-sm">Home</span>
+              <button className=" text-black px-4 py-2">
+                <Image
+                  src="https://res.cloudinary.com/guffenix/image/upload/f_auto,q_auto/v1/flappymode/earnbtn"
+                  alt="earn Game"
+                  width={80}
+                  height={80}
+                  className="w-auto h-14"
+                  style={{
+                    filter: 'drop-shadow(1px 5px 5px rgb(147 197 253 / 0.6))',
+                  }}
+                />
+              </button>
             </a>
           </li>
           <li>
             <a
               href="#"
-              className="flex flex-col items-center gap-1 text-zinc-400 transition-colors hover:text-zinc-100"
-            >
-              <Star className="h-6 w-6" />
-              <span className="text-sm">Star</span>
-            </a>
+              className="flex flex-col items-center gap-1 rounded-full bg-[#DFFE00]/0 p-4 text-black w-28 h-10"
+            ></a>
           </li>
-          <li className="relative -mt-8">
-            <a
-              href="#"
-              className="flex flex-col items-center gap-1 rounded-full bg-[#DFFE00] p-4 text-black shadow-lg"
-            >
-              <Flame className="h-6 w-6" />
-              <span className="text-sm font-medium">Play</span>
-            </a>
-          </li>
+
           <li>
             <a
               href="#"
               className="flex flex-col items-center gap-1 text-zinc-400 transition-colors hover:text-zinc-100"
             >
-              <Coins className="h-6 w-6" />
-              <span className="text-sm">Earn</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex flex-col items-center gap-1 text-zinc-400 transition-colors hover:text-zinc-100"
-            >
-              <Users className="h-6 w-6" />
-              <span className="text-sm">Friends</span>
+              <button className=" text-black px-4 py-3">
+                <Image
+                  src="https://res.cloudinary.com/guffenix/image/upload/f_auto,q_auto/v1/flappymode/rankingbtn"
+                  alt="ranking Game"
+                  width={80}
+                  height={80}
+                  className="w-auto h-14"
+                  style={{
+                    filter: 'drop-shadow(1px 5px 5px rgb(147 197 253 / 0.6))',
+                  }}
+                />
+              </button>
             </a>
           </li>
         </ul>
