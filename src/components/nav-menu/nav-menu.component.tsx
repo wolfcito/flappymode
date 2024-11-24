22c66f614b88da99107bfb9c82863a2c353c329a
Luis Fernando <guffenix@gmail.com>
2024-11-24 01:02:29 -0500

    chore: update navbar menu

--- src/components/nav-menu/nav-menu.component.tsx
+++ src/components/nav-menu/nav-menu.component.tsx
@@ -1,64 +1,63 @@
+'use client'
+
+import { usePathname } from 'next/navigation' // Cambia useRouter por usePathname
+import { Crown, Shell, ListCheck } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function NavMenu() {
-  return (
-    <div
-      className="fixed bottom-0 left-0 right-0 bg-white/10"
-      style={{
-        backgroundImage: `url('https://res.cloudinary.com/guffenix/image/upload/f_auto,q_auto/v1/flappymode/bgmarine')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
-      <nav className="mx-auto max-w-md bg-zinc-900 backdrop-blur-sm py-2">
-        <ul className="flex items-center justify-around">
-          <li>
-            <a
-              href="#"
-              className="flex flex-col items-center text-zinc-400 transition-colors hover:text-zinc-100"
-            >
-              <button className=" text-black px-4 py-2">
-                <Image
-                  src="https://res.cloudinary.com/guffenix/image/upload/f_auto,q_auto/v1/flappymode/earnbtn"
-                  alt="earn Game"
-                  width={80}
-                  height={80}
-                  className="w-auto h-14"
-                  style={{
-                    filter: 'drop-shadow(1px 5px 5px rgb(147 197 253 / 0.6))',
-                  }}
-                />
-              </button>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex flex-col items-center gap-1 rounded-full bg-[#DFFE00]/0 p-4 text-black w-28 h-10"
-            ></a>
-          </li>
+  const pathname = usePathname()
 
-          <li>
-            <a
-              href="#"
-              className="flex flex-col items-center gap-1 text-zinc-400 transition-colors hover:text-zinc-100"
-            >
-              <button className=" text-black px-4 py-3">
-                <Image
-                  src="https://res.cloudinary.com/guffenix/image/upload/f_auto,q_auto/v1/flappymode/rankingbtn"
-                  alt="ranking Game"
-                  width={80}
-                  height={80}
-                  className="w-auto h-14"
-                  style={{
-                    filter: 'drop-shadow(1px 5px 5px rgb(147 197 253 / 0.6))',
-                  }}
-                />
-              </button>
-            </a>
-          </li>
-        </ul>
-      </nav>
+  const isActive = (path: string) => pathname === path
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 bg-zinc-900 border-t border-zinc-800">
+      <div className="flex justify-around py-4 relative">
+        <div className="absolute bottom-4 left-4">
+          <Link href="/">
+            <Image
+              src="https://res.cloudinary.com/guffenix/image/upload/f_auto,q_auto/v1/flappymode/homebtn"
+              alt="home"
+              width={100}
+              height={100}
+              className="w-auto h-24"
+              style={{
+                filter: 'drop-shadow(1px 5px 5px rgb(147 197 253 / 0.6))',
+              }}
+            />
+          </Link>
+        </div>
+
+        <div className="flex justify-around flex-1 pl-24">
+          <Link
+            href="/"
+            className={`flex flex-col items-center ${
+              isActive('/') ? 'text-[#DFFE00]' : 'text-gray-400'
+            }`}
+          >
+            <Shell className="h-6 w-6" />
+            <span className="text-sm">Game</span>
+          </Link>
+          <Link
+            href="/earn"
+            className={`flex flex-col items-center ${
+              isActive('/earn') ? 'text-[#DFFE00]' : 'text-gray-400'
+            }`}
+          >
+            <ListCheck className="h-6 w-6" />
+            <span className="text-sm">Task</span>
+          </Link>
+          <Link
+            href="/ranking"
+            className={`flex flex-col items-center ${
+              isActive('/ranking') ? 'text-[#DFFE00]' : 'text-gray-400'
+            }`}
+          >
+            <Crown className="h-6 w-6" />
+            <span className="text-sm">Rankings</span>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
